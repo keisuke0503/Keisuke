74bc1e819afb004b8c7b01095ba10445295760ae
keisuke0503 <87107829+keisuke0503@users.noreply.github.com>
2021-07-19 22:43:13 +0800

    Add files via upload

---
+++ Struct作業.cpp
@@ -0,0 +1,7 @@
+C:\Users\88690\OneDrive\桌面\Struck作業.cpp	In function 'void enterData(ASS*, int)':
+31	16	C:\Users\88690\OneDrive\桌面\Struck作業.cpp	[Error] expected type-specifier before 'Point'
+31	54	C:\Users\88690\OneDrive\桌面\Struck作業.cpp	[Error] expected ',' or ';' before ')' token
+C:\Users\88690\OneDrive\桌面\Struck作業.cpp	In function 'int main()':
+78	22	C:\Users\88690\OneDrive\桌面\Struck作業.cpp	[Error] 's' was not declared in this scope
+83	16	C:\Users\88690\OneDrive\桌面\Struck作業.cpp	[Error] 'Ass' was not declared in this scope
+83	23	C:\Users\88690\OneDrive\桌面\Struck作業.cpp	[Error] expected primary-expression before ')' token
